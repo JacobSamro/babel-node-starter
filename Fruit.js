4b768f49162f9bf681efceedb1edfee4a25622cf
Jacob Samro <jacobsamro@outlook.com>
2019-06-07 14:27:38 +0530

    Basic Setup 💪

---
+++ Fruit.js
@@ -0,0 +1,10 @@
+export default class Fruit {
+  constructor() {
+    console.log("Fruit Created")
+  }
+
+  name(name) {
+    console.log(`My name is ${name}`)
+  }
+}
+
---
+++ app.js
@@ -0,0 +1,6 @@
+import Fruit from './Fruit'
+
+
+let fruit = new Fruit()
+
+fruit.name("Apple")
